fca22558f90246f09582baf4db28b6c6cce59a8e
ajou20658 <kwy1379@ajou.ac.kr>
2024-01-22 00:50:37 +0900

    Spring-Security[~2.8]

---
+++ src/main/java/com/example/demo/config/ProjectConfig.java
@@ -0,0 +1,41 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+@EnableWebSecurity
+@Configuration
+public class ProjectConfig{
+    @Bean
+    public UserDetailsService userDetailsService(){
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("john")
+                .password("12345")
+                .authorities("read")
+                .build();
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(requestMatcherRegistry->requestMatcherRegistry
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+
+}
