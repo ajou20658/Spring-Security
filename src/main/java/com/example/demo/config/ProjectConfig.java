c33344ca3e71df214074572a59cb7584b81792c5
ajou20658 <kwy1379@ajou.ac.kr>
2024-01-23 02:57:01 +0900

    Spring-Security[~3.15] passwordEncoder 해쉬없이 UserDetailsService 커스텀 재정의 이때 loadUserByUsername은 유저 조회를 위한 메서드

--- src/main/java/com/example/demo/config/ProjectConfig.java
+++ src/main/java/com/example/demo/config/ProjectConfig.java
@@ -1,41 +1,26 @@
 package com.example.demo.config;
 
+import com.example.demo.model.User;
+import com.example.demo.service.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
-@EnableWebSecurity
+
+import java.util.List;
+
 @Configuration
-public class ProjectConfig{
+public class ProjectConfig {
     @Bean
     public UserDetailsService userDetailsService(){
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-        userDetailsService.createUser(user);
-
-        return userDetailsService;
+        UserDetails u = new User("john","12345","read");
+        List<UserDetails> users = List.of(u);
+        return new InMemoryUserDetailsService(users);
     }
     @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
     }
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(requestMatcherRegistry->requestMatcherRegistry
-                        .anyRequest().permitAll()
-                );
-        return http.build();
-    }
-
 }
